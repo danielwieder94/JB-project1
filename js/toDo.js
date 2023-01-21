886de8364301f5dde86f237d925c15395f6f252c
daniel wieder <danielwieder94@gmail.com>
2023-01-21 15:01:01 +0200

    almost final

--- js/toDo.js
+++ js/toDo.js
@@ -1,78 +1,46 @@
-noteArray = [];
-const notesContainer = document.getElementById("notes");
+let noteArray = JSON.parse(localStorage.getItem("noteData")) || [];
 
-const checkForData = () => {
-  console.log(localStorage.getItem("noteData"));
-  noteArray = JSON.parse(localStorage.getItem("noteData"));
-  console.log(noteArray);
-  addNote();
+const prettifyDate = (noteDate) => {
+  return new Date(noteDate).toLocaleDateString();
 };
 
-Date.prototype.getNewDate = function () {
-  let myDate = this.toLocaleDateString(noteDate).split("/");
-  // myDate = myDate.split("/");
-  return `${myDate[1]}/${myDate[0]}/${myDate[2]}`;
+const insertData = () => {
+  const notesContainer = document.getElementById("notes");
+  notesContainer.innerHTML = noteArray
+    .map((item) => {
+      return `<div class="note">
+      <span class="deleteNote" onclick="deleteItem(this, ${noteArray.indexOf(
+        item
+      )})"><i class="fa-solid fa-xmark"></i></span>
+      <div class="textWrap">
+        <div class="noteText">${item.noteText}</div>
+      </div>
+      <div class="date">${prettifyDate(item.noteDate)}</div>
+      <div class="time">${item.noteTime}</div>
+    </div>`;
+    })
+    .join("");
 };
 
-// const prettifyDate = () => {
-//   let myDate = new Date(noteDate.value).toLocaleDateString();
-//   let newDate = myDate.split("/");
-//   return `${newDate[1]}/${newDate[0]}/${newDate[2]}`;
-// };
-
-const deleteItem = (element, newNote) => {
-  element.remove();
-  noteArray = noteArray.filter((item) => item.element !== element);
-  localStorage.removeItem("newNote");
-  newNote.element = null;
-  console.log(noteArray);
+const deleteItem = (element, index) => {
+  element.parentNode.remove();
+  noteArray.splice(index, 1);
+  localStorage.setItem("noteData", JSON.stringify(noteArray));
 };
+
+insertData();
 
 const addNote = () => {
-  const noteText = document.getElementById("noteText").value;
-  const noteDate = document.getElementById("noteDate").value;
-  const noteTime = document.getElementById("noteTime").value;
-
-  //Create note and its content
-  const newNote = {
-    text: noteText,
-    noteDate: noteDate,
-    noteTime: noteTime,
-    element: document.createElement("div"),
+  const note = {
+    noteText: document.getElementById("noteText").value,
+    noteDate: document.getElementById("noteDate").value,
+    noteTime: document.getElementById("noteTime").value,
   };
-  newNote.element.classList.add("note");
-
-  const noteTextWrap = document.createElement("div");
-  noteTextWrap.classList.add("textWrap");
-
-  const newNoteText = document.createElement("div");
-  newNoteText.classList.add("noteText");
-  newNoteText.innerHTML = noteText;
-  noteTextWrap.appendChild(newNoteText);
-  newNote.element.appendChild(noteTextWrap);
-
-  const newNoteDate = document.createElement("div");
-  newNoteDate.classList.add("date");
-  newNoteDate.innerHTML = `${getNewDate(this)}`;
-  newNote.element.appendChild(newNoteDate);
-
-  const newNoteTime = document.createElement("div");
-  newNoteTime.classList.add("time");
-  newNoteTime.innerHTML = noteTime;
-  newNote.element.appendChild(newNoteTime);
-
-  //create delete option
-  const deleteNote = document.createElement("button");
-  deleteNote.classList.add("deleteNote");
-  deleteNote.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
-  deleteNote.addEventListener("click", () => {
-    deleteItem(newNote.element, newNote);
-  });
-  newNote.element.appendChild(deleteNote);
-
-  notesContainer.appendChild(newNote.element);
-  noteArray.push(newNote);
-  console.log(noteArray);
-  localStorage.setItem("noteData", JSON.stringify(noteArray));
+  noteArray.push(note);
   document.getElementById("noteForm").reset();
+  localStorage.setItem("noteData", JSON.stringify(noteArray));
+  insertData();
+  const lastAddedNote = document.querySelector(".note:last-of-type");
+  lastAddedNote.classList.add("new-note");
+  setTimeout(() => lastAddedNote.classList.remove("new-note"), 500);
 };
